90c017480425337c3a3a0a5b0315045434654b8e
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-04-02 08:15:51 -0400

    Clean up

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,11 +1,8 @@
 package pkg
 
 import (
-	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 )
 
@@ -30,28 +27,4 @@ func Write(file string, data []byte, perm os.FileMode) error {
 	err := ioutil.WriteFile(file, data, perm)
 
 	return err
-}
-
-func Cmd(file string) {
-
-	tcmd := `go get github.com/axw/gocov/gocov \
-go get -u github.com/mchirico/date/parse \
-go get gopkg.in/yaml.v2 \
-go get github.com/spf13/cobra \
-go get github.com/mitchellh/go-homedir \
-go get github.com/spf13/viper 
-
-`
-	tcmd = "go"
-
-	scmd := fmt.Sprintf("export GOPATH=./%s;%s", file,tcmd)
-
-	fmt.Println(scmd)
-
-	out, err := exec.Command("/bin/bash", "-c", scmd).Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("The date is %s, %s\n", out,tcmd)
-
 }
