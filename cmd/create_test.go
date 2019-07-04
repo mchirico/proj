35b3ed3ca688df5ced44f362f15e6359ab887a56
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-07-04 18:05:58 -0400

    Better tests

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -8,7 +8,20 @@ import (
 	"testing"
 )
 
+func FileIn(file, pwd string) bool {
+	files := util.ListFiles(pwd)
+	for _, value := range files {
+
+		if strings.Contains(value, file) {
+			return true
+		}
+	}
+	return false
+}
+
 func TestCreateFile(t *testing.T) {
+
+	t.Parallel()
 
 	defer util.NewTlib().ConstructDir()()
 
@@ -24,9 +37,30 @@ func TestCreateFile(t *testing.T) {
 		t.Fatalf("No data...")
 	}
 
-	flist := util.ListFiles(util.PWD())
-	if len(flist) < 15 {
-		t.Fatalf("Did not create files...")
+	tests := []struct {
+		name string
+		file string
+	}{
+		{name: "test Dockerfile", file: "Dockerfile"},
+		{name: "test Azure pipeline", file: "azure-pipelines.docker.yml"},
+		{name: "test travis", file: ".travis.yml"},
+		{name: "test README.md", file: "README.md"},
+		{name: "test recreateWithCobra.sh", file: "recreateWithCobra.sh"},
+		{name: "test setpath", file: "setpath"},
+		{name: "test .gitignore", file: ".gitignore"},
+		{name: "test start.sh", file: "start.sh"},
+		{name: "test github.com", file: "github.com"},
+		{name: "test src", file: "src"},
+		{name: "test mchirico", file: "mchirico"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if !FileIn(tc.file, util.PWD()) {
+				t.Fatalf("can't find: %s\n", tc.file)
+			}
+
+		})
 	}
 
 }
